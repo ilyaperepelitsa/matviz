ce8fe3582c33ce7269afcda10f9a48b968ea0389
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-01-30 09:09:35 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -47,5 +47,7 @@ def explode(df, lst_cols, fill_value=''):
 latlon = pd.DataFrame(total_exploded.step_location_list.str.split(',',1).tolist(),
                                    columns = ['lon','lat'])
 
-                                   
+total_exploded = explode(total_set, ["street_for_each_step", "distance_per_step", "travel_time_per_step"
+                   , "step_maneuvers", "step_direction", "step_location_list"])
+                   
 total_exploded = pd.concat([total_exploded, latlon], axis=1, join='inner')
