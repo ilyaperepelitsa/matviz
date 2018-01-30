458f119d5fde5d0b77416f1c7ea066b8707f872c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-01-30 09:08:00 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -11,3 +11,8 @@ from matplotlib.figure import Figure
 first_set = pd.read_csv("~/quant/fastest_routes_train_part_1.csv")
 second_set = pd.read_csv("~/quant/fastest_routes_train_part_2.csv")
 total_set = pd.concat([first_set, second_set])
+
+
+for col in ["street_for_each_step", "distance_per_step", "travel_time_per_step",
+                "step_maneuvers", "step_direction", "step_location_list"]:
+    data_play.loc[:,col] = data_play.loc[:,col].str.split('|').tolist()
