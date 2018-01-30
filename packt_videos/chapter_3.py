ed81c40a8ab6dff5b8540ccbfcf88ade6ddce073
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-01-30 09:05:13 -0500

    Updated chapter_3.py.

---
+++ packt_videos/chapter_3.py
@@ -0,0 +1,6 @@
+from IPython.display import display_png
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
