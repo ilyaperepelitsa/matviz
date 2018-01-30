f37342c79ca9e05764f6b334361c47b7b7331eb1
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-01-30 00:24:04 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
+
+first_set = pd.read_csv("~/quant/fastest_routes_train_part_1.csv")
+second_set = pd.read_csv("~/quant/fastest_routes_train_part_2.csv")
