b741df0d96bb2126f85191b86bec4598a7b705f0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-13 23:05:10 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -64,5 +64,5 @@ from matplotlib.figure import Figure
 total_exploded = pd.read_csv("~/quant/exploded.csv")
 
 
-total_exploded = ['pooh', 'rabbit', 'piglet', 'Christopher']
+total_exploded.
 >>> np.random.choice(aa_milne_arr, 5, p=[0.5, 0.1, 0.1, 0.3])
