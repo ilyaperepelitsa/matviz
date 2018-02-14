bb7b88b41fc5770ea257640c55820d4db9be7c9d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 01:12:11 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -94,7 +94,7 @@ plt.show()
 
 
 # sample_trips = np.random.choice(trip_ids, 30000)
-for aspect in ["equal", ]
+for aspect in ["equal", ‘auto’]
 for x in range(10, 50):
     plt.figure(figsize = (18.5, 10.5))
 
