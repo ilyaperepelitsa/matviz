af2dae08d34a4d714008a4c1320898f043ea8611
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 15:54:38 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -118,7 +118,7 @@ for aspect in ["equal", "auto"]:
     # plt.show()
 streets = np.random.choice(sample_30k["starting_street"].unique(), 5)
 for street in streets:
-    print(sample_30k.loc[sample_30k["starting_street"].isin([street]), :].shape)
+    sample_30k.loc[sample_30k["starting_street"].isin([street]), :].shape)
 
 
 
