683e73b5b6480cd0aa0d5b7caa636ca468b2469f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 16:11:34 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -130,27 +130,3 @@ plt.show()
 
 plt.figure(figsize = (18.5, 10.5))
 # x = np.column_stack([sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]] for street in streets])
-
-
-spread = np.random.rand(50) * 100
-center = np.ones(25) * 50
-flier_high = np.random.rand(10) * 100 + 100
-flier_low = np.random.rand(10) * -100
-data = np.concatenate((spread, center, flier_high, flier_low), 0)
-
-
-spread
-center
-flier_high
-flier_low
-data
-data.shape
-data.shape = (-1, 1)
-data
-
-
-data[::2]
-x = list(range(100))
-x[::2, 0]
-x[::3]
-x[10:40:6]
