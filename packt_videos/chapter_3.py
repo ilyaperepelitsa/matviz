158d0882d6fa482175f8770d00eb887e985471eb
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 01:12:02 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -110,6 +110,6 @@ for x in range(10, 50):
     # invert_y
     # plt.xlim(invert_x)
     # plt.ylim(invert_y)
-    plt.axes().set_aspect(aspect, 'datalim')
+    plt.axes().set_aspect(aspect)
     plt.savefig('/Users/ilyaperepelitsa/quant/taxi_test_{:d}.jpg'.format(x), dpi=300)
     # plt.show()
