0c5a4db33343422f893edc67b15548a53d4ac2ac
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 17:04:40 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -133,7 +133,7 @@ data = [[street,
     [sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]].values],
     sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]].median().values[0]]
     for street in streets]
-plt.boxplot([street_data[0] for street_data in data])
+plt.boxplot([street_data[1] for street_data in data])
 [street_data[0] for street_data in data]
 names = streets
 data
