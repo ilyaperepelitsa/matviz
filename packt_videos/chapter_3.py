720aa9c05ea9b773de72dbaf660aa7855dd134ea
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 18:40:37 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -153,6 +153,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
 
     if order_by = "median":
         order_func = np.median
+    eli
 
     data = [[data_group_var,
         [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
