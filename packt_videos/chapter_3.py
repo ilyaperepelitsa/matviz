2c719b11a3337ce6cb12f51ec6041135c8305395
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 15:57:39 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -122,7 +122,8 @@ streets = np.random.choice(sample_30k["starting_street"].unique(), 5)
 plt.figure(figsize = (18.5, 10.5))
 for street in streets:
     plt.boxplot(sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]].values)
-names = 
+names = streets
+
 plt.show()
 
 
