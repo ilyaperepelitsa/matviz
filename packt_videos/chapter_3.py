e90611742408fd54212c29ffb7b03b4126938714
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 18:55:35 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -165,10 +165,3 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
 
 order_func = stats.mode[0][0]
 order_func([1, 1, 2, 3])
-
-def mode_pew(a):
-    (_, idx, counts) = np.unique(a, return_index=True, return_counts=True)
-    index = idx[np.argmax(counts)]
-    mode = a[index]
-
-print(mode_pew([1, 2, 3, 3, 4]))
