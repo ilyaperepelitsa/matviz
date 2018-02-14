92b9d0b425a6a6ec4f96e9be7486788c284e50bd
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 18:57:10 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -163,4 +163,5 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
         [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
         data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].order_func().values[0]]
         for data_group_var in data_group]
-    data_sorted = sorted(data, key=lambda street: street[2])
+
+    data_sorted = sorted(data, key=lambda sort_var: street[2])
