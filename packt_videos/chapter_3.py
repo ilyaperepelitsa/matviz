1a76900ad769a6b7e7f3072e1047a39fc68c9af6
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 18:38:09 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -148,8 +148,10 @@ plt.show()
 plt.figure(figsize = (18.5, 10.5))
 # x = np.column_stack([sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]] for street in streets])
 
-def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, order = "desc", order_by = "me"):
+def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, order = "desc", order_by = "median"):
     plt.figure(figsize = (23.5, 10.5))
+
+    
 
     data = [[data_group_var,
         [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
