80e5b679f6e08b023ed5f34e0f4fb2523adb8f28
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 00:44:41 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -98,7 +98,7 @@ for i in range(0, 10):
     # print(i)
     rand_index = np.random.choice(sample_30k["id"].unique(), 1)
 
-    plt.plot(sample_30k.loc[sample_30k["id"].isin(rand_index), "lat"],
+    plt.plot(sample_30k.loc[sample_30k["id"].isin(rand_index), "lon"],
                 sample_30k.loc[sample_30k["id"].isin(rand_index), "lon"])
 
 plt.show()
