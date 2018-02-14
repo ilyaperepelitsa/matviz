fdea1bfa8a4a796fd04b3e5e35f03df7b592320a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 16:06:53 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -137,3 +137,6 @@ center = np.ones(25) * 50
 flier_high = np.random.rand(10) * 100 + 100
 flier_low = np.random.rand(10) * -100
 data = np.concatenate((spread, center, flier_high, flier_low), 0)
+
+
+s
