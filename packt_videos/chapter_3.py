26b5bc9d473826ff06462243d4af76474862e2fc
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-13 23:34:37 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -77,6 +77,5 @@ plt.show()
 plt.hist(sample_30k["total_travel_time"])
 plt.show()
 
-fig = matplotlib.pyplot.gcf()
-fig.set_size_inches(18.5, 10.5)
+plt.gcset_size_inches(18.5, 10.5)
 fig.savefig('test2png.png', dpi=100)
