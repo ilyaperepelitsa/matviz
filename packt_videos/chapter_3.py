4d8dcf2de888fb4178d99486741ca45b4879edec
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 00:51:02 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -95,7 +95,7 @@ plt.show()
 
 # sample_trips = np.random.choice(trip_ids, 30000)
 plt.figure(figsize = (18.5, 10.5))
-for i in range(0, 10):
+for i in range(0, 3):
     # print(i)
     rand_index = np.random.choice(sample_30k["id"].unique(), 1)
 
