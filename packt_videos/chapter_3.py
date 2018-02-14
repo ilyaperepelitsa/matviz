6f57a2b4b1ac7ab71222b76b0dc3e6ac5e13100d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 01:00:12 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -107,5 +107,5 @@ for i in range(0, 100):
 # invert_y
 # plt.xlim(invert_x)
 # plt.ylim(invert_y)
-plt.savefig('/Users/ilyaperepelitsa/taxi_test.jpg', dpi=100)
+plt.savefig('/Users/ilyaperepelitsa/quant/taxi_test.jpg', dpi=100)
 plt.show()
