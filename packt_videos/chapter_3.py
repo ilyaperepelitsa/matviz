65ac4cef095f059cf609edd54c7d8a8b90a6dceb
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 16:15:50 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -124,7 +124,7 @@ plt.figure(figsize = (18.5, 10.5))
 #     plt.boxplot(sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]].values)
 plt.boxplot([[sample_30k.loc[sample_30k["starting_street"].isin([street]), ["travel_time_per_step"]].values] for street in streets])
 # names = streets
-# plt.xticks(range(1, len(names) +1), names)
+plt.xticks(range(1, len(names) +1), names)
 plt.show()
 
 
