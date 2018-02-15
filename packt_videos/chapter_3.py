835e69039d19856dab39bdfd086f7fb43b11136f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:13:47 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -173,6 +173,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
         outlier_mark = "c"
     else:
         outlier_mark = ""
-    
+
+    if
 
     plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, 0)
