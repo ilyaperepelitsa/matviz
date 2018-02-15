acc9a8e8598f409cc5c12ea83a9bf44620597d04
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:17:41 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -180,8 +180,11 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
         invert_value = 0
 
     plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value)
-
     names = [data_data[0] for data_data in data_sorted]
+    if invert_axes:
+        invert_value = 1
+    else:
+        invert_value = 0
     plt.yticks(range(1, len(names) +1), names)
     plt.show()
 
