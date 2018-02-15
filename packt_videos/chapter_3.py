76e8a6e05ccd84e27adc7c2e42e609705d7d748f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:24:57 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -153,7 +153,10 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
 
 
     if order_by == "median":
-        order_func = np.median
+        data = [[data_group_var,
+            [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
+            data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].mean().values[0]]
+            for data_group_var in data_group]
     elif order_by == "mean":
         order_func = np.mean
     else:
