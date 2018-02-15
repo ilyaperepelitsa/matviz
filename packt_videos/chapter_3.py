8d096e5650117276c33cf9d0304ba09de9fd62bf
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 23:35:04 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -231,7 +231,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
     plt.suptitle(title + "\n",
                     # y=1.03, x = 0.35,
                  fontsize=18, color=keycolor,
-                 horizontalalignment="right")
+                 horizontalalignment="center")
 
     # Graph subtitle
     plt.title(subtitle + "\n\n",
