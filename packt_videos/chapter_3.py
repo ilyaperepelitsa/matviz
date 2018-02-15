d9e4285df2606d3866862b45a80a85a1969f6eb9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-15 00:04:56 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -244,7 +244,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                  loc = "left", fontsize=18,
                 color=keycolor)
 
-    # plt.savefig('/Users/ilyaperepelitsa/quant/pewpewpew.jpg', dpi=300)
+    plt.savefig('/Users/ilyaperepelitsa/quant/pewpewpew.jpg', dpi=300)
     plt.show()
 
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
