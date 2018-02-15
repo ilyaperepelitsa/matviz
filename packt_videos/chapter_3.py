1dc0083d25c9e2be02d3fb5e87357c5905af3c24
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:27:03 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -190,4 +190,5 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
     plt.show()
 
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
-                    data_group = "starting_street", invert_axTrue, "desc", "median", False)
+                    data_group = "starting_street", invert_axes = True,
+                    "desc", "median", False)
