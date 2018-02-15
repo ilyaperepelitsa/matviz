29bfd58d370be392afe218b7f6e7e82130d1603c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 23:45:18 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -230,8 +230,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
     # Graph title
     plt.suptitle(title,
                 y=1.02, x = 0.1,
-                horizontalalignment="right",
-                # horizontalalignment="left",
+                # horizontalalignment="right",
+                horizontalalignment="left",
                 # horizontalalignment="center",
                  fontsize=18, color=keycolor,
                  )
