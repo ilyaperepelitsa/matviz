2b14a337a146971a919a850ac1e5ecec02e50ec3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 22:56:28 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -159,7 +159,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                             order = "desc", order_by = "median",
                             show_outliers = True, get_top = False,
                             title = None,
-                            ):
+                            subtitle = None):
 
 
     labels_to_filter = data_in.groupby([data_group]).size().sort_values(ascending = False).index
