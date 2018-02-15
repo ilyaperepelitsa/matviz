c3cecf029312c3fd1c7942e76ef661b43663e17a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 22:49:08 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -211,6 +211,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
     else:
         plt.xticks(range(1, len(names) +1), names)
 
+    
     plt.show()
 
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
