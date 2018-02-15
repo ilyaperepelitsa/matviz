bbc01f6ae417c3ff2598caa07f7d2caed208ae1f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 23:47:08 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -253,7 +253,7 @@ plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
                     data_group = "starting_street", invert_axes = True,
                     order = "desc", order_by = "median", show_outliers = False,
-                    get_top = 10)
+                    get_top = 20)
 
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
                     data_group = "starting_street", invert_axes = True,
