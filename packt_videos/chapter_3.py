bb742bb7da2b795feeb448598b4a2145dfc0ff18
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 22:40:05 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -171,8 +171,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
         canvas_height = 0.2*len(group_labels)
         canvas_width = 10
     else:
-        canvas_height = 0.2*len(group_labels)
-        canvas_width = 10
+        canvas_height = 10
+        canvas_width =  0.2*len(group_labels)
 
     if order_by == "mean":
         data = [[data_group_var,
