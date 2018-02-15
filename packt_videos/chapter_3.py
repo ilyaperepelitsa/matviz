a520545765dbeec8b8ef996d5f7139b6cf662508
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:33:09 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -159,8 +159,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
             for data_group_var in data_group]
     else:
         data = [[data_group_var,
-            [data_in.loc[data_in[data_group].isin(data_group_var]), [data_num]].values],
-            data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].median().values[0]]
+            [data_in.loc[data_in[data_group].isin(data_group_var), [data_num]].values],
+            data_in.loc[data_in[data_group].isin(data_group_var), [data_num]].median().values[0]]
             for data_group_var in data_group]
 
 
