e2eb9960546820ddf4e0a1c8573663e5ada94a3e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 23:01:39 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -159,7 +159,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                             order = "desc", order_by = "median",
                             show_outliers = True, get_top = False,
                             keycolor = "#232626",
-                            title = "Have your title here.",
+                            title = "Don't forget",
                             subtitle = None,
                             xlabel = None,
                             ylabel = None):
