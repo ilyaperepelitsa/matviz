ede82d65aa89dd3df45cd4435baa17971e18cc66
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 19:44:47 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -179,16 +179,16 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False, ord
     else:
         invert_value = 0
 
-    # plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value)
+    plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value)
     names = [data_data[0] for data_data in data_sorted]
     # print(data_sorted)
     # print(names)
-    # if invert_axes:
-    #     plt.yticks(range(1, len(names) +1), names)
-    # else:
-    #     plt.xticks(range(1, len(names) +1), names)
-    #
-    # plt.show()
+    if invert_axes:
+        plt.yticks(range(1, len(names) +1), names)
+    else:
+        plt.xticks(range(1, len(names) +1), names)
+
+    plt.show()
 
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
                     data_group = "starting_street", invert_axes = True,
