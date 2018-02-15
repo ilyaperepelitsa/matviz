9387242a1a2c425c41d16bd8f0bedc2fc1d74e48
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-14 21:59:25 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -197,7 +197,7 @@ plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
 
 
 for i in sample_30k.groupby(["starting_street"]).size().sort_values(ascending = False).index:
-    
+    print(i)
 dir(pd.Series)
 sample_30k.groupby(["starting_street"]).size().sort_values(ascending = False)
 # .reset_index(name='counts')
