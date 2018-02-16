b09619b763236f089596d870dd1380f302f35c20
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-16 00:04:23 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -282,7 +282,7 @@ sample_30k.groupby(["starting_street"]).size().sort_values(ascending = False)
 # .reset_index(name='counts')
 
 sample_30k.loc[sample_30k["starting_street"].isin(["Broadway"]), ["travel_time_per_step"]].\
-sort_values(by = ["travel_time_per_step"],ascending = False)
+sort_values(by = ["travel_time_per_step"],ascending = False)*
 if order_by == "mean":
     data = [[data_group_var,
         [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
