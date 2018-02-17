3ad2e4a16c3b4ce4763097b74fef73eabc6e483e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 02:21:18 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -221,7 +221,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
     matplotlib.rc('axes', edgecolor=keycolor)
     plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
                 whiskerprops = {'color': "#6dedd1", 'linewidth' : 10, 'zorder' : 0 },
-                medianprops = {'color': keycolor}
+                medianprops = {'color': keycolor},
+                boxprops = {'color': base_color, 'facecolor': base_color}
                 )
     names = [data_data[0] for data_data in data_sorted]
     # print(data_sorted)
