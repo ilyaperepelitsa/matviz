95af723495dce29f041bb9d16456f2e2f4d36c5d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 02:03:30 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -219,7 +219,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
         invert_value = 1
 
     matplotlib.rc('axes', edgecolor=keycolor)
-    plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value)
+    plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
+                )
     names = [data_data[0] for data_data in data_sorted]
     # print(data_sorted)
     # print(names)
