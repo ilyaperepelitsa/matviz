6605107425275d821d4655620f5bb74788fa0c27
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 17:37:21 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -16,7 +16,7 @@ cmap = lambda x: {x, next(color_cycler)["color"]}
 
 pew = []
 for i in range(1, 5):
-    color = cmap(i)
+    plot_cols[data_data[0]] = cmap(data_data[0])[data_data[0]]
     print(color)
 
 for i in range(1, 5):
