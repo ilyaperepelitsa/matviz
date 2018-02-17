e5000d230e444702bfd1052f532c62b2dade700a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 02:36:04 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -220,6 +220,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
 
     matplotlib.rc('axes', edgecolor=keycolor)
     plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
+        
                 whiskerprops = {'color': "#8999b2", 'linewidth' : 9.5, 'zorder' : 0},
                 medianprops = {'color': keycolor, 'linewidth' : 1.5},
                 capprops = {'color': keycolor, 'linewidth' : 4},
