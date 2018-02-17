4126fbbf0f3b765b8cd87144fab502900d62e51c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 03:17:13 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -226,7 +226,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 # capprops = {'color': keycolor, 'linewidth' : 4},
                 showcaps=False,
 
-                boxprops = {'color': None, 'facecolor': '#353535', 'zorder' : 999}
+                boxprops = {'color': "#8999b2", 'facecolor': '#353535', 'zorder' : 999}
                 # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
                 )
     names = [data_data[0] for data_data in data_sorted]
