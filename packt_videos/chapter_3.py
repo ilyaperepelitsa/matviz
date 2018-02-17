ea9463e2472da1ee276131f1006ecde8e6ea876b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 17:27:51 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -236,9 +236,9 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
 
     plot_cols = []
     for data_data in data_sorted:
-         for data_data in data_sorted
-        plot_cols.append(cmap("pew"))
-        print(color)
+         # for data_data in data_sorted
+        plot_cols.append(cmap(data_data[0]))
+        # print(color)
 
 
     for data_data in data_sorted:
