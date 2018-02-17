176184888577d50b5d9fc73d74ca350ee071e464
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 17:31:10 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -234,7 +234,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
 
     matplotlib.rc('axes', edgecolor=keycolor)
 
-    plot_cols = []
+    plot_cols = {}
     for data_data in data_sorted:
          # for data_data in data_sorted
         plot_cols.append(cmap(data_data[0]))
