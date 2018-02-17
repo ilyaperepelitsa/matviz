59a0c42ba6a814e0fcd56af516c5c671fad89b1a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 02:44:46 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -223,7 +223,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 patch_artist = True,
                 whiskerprops = {'color': "#8999b2", 'linewidth' : 9.5, 'zorder' : 0},
                 medianprops = {'color': "#ffffff", 'linewidth' : 3, 'zorder' : 1000},
-                capprops = {'color': keycolor, 'linewidth' : 4},
+                # capprops = {'color': keycolor, 'linewidth' : 4},
                 boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
                 )
     names = [data_data[0] for data_data in data_sorted]
