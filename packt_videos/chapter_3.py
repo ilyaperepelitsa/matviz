3659aec1ed0871298302249ff424ec392a522b2b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-17 17:24:56 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -246,7 +246,16 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
                 )
 
-    
+    # plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
+    #             patch_artist = True,
+    #             whiskerprops = {'color': "#8999b2", 'linewidth' : 9.5, 'zorder' : 0},
+    #             medianprops = {'color': "#ffffff", 'linewidth' : 3, 'zorder' : 1000},
+    #             # capprops = {'color': keycolor, 'linewidth' : 4},
+    #             showcaps=False,
+    #
+    #             boxprops = {'color': keycolor, 'facecolor': '#5e5757', 'zorder' : 999}
+    #             # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
+    #             )
     names = [data_data[0] for data_data in data_sorted]
     # print(data_sorted)
     # print(names)
