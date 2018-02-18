9df47670d72c799eee335f17041294c4135612c9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 16:59:39 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -16,10 +16,7 @@ from random import shuffle
 palette = [['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
  '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']]
 
-palette = [['#bcc19c',
- '#c19c9e',
- '#c19cbc',
- '#c1a09c',
+palette = [['#bcc19c','#c19c9e','#c19cbc','#c1a09c',
  '#c19cac',
  '#9c9dc1',
  '#9cb0c1',
