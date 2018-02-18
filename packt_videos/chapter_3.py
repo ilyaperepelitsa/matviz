a7f650b5ae375c7a48130db2cf4e2235e9d3583a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 14:53:23 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -170,7 +170,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
         invert_value = 1
 
     matplotlib.rc('axes', edgecolor=keycolor)
-    pew = plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
+    boxes = plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
                 patch_artist = True,
                 whiskerprops = {'color': "#8999b2", 'linewidth' : 9.5, 'zorder' : 0},
                 medianprops = {'color': "#ffffff", 'linewidth' : 3, 'zorder' : 1000},
@@ -182,7 +182,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 )
 
     plot_cols = dict()
-    for box in data_sorted:
+    for box in pew["boxes"]:
          # plot_cols[data_data[0]] = cmap(data_data[0])[1]
          print(cmap())
 
