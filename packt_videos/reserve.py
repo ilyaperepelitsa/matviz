6e286d316ba533937cc336f257909accdc3ccbbb
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 01:42:50 -0500

    Updated reserve.py.

---
+++ packt_videos/reserve.py
@@ -0,0 +1,7 @@
+
+    plot_cols = dict()
+    for data_data in data_sorted:
+         # for data_data in data_sorted
+         # print(data_data[0])
+         # plot_cols[str(i)] = cmap(str(i))[1]
+         plot_cols[data_data[0]] = cmap(data_data[0])[1]
