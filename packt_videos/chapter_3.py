8169bf4dc1a4336b415de15dd3e5f82b1301bd96
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 16:59:12 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -14,9 +14,7 @@ from collections import defaultdict
 from random import shuffle
 
 palette = [['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
- '#918e5f','#bf8765','#4e3f63',
-'#824a6b',
- '#547a63',
+ '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63',
  '#6b7a54']]
 
 palette = ["#c19c9e", "#c19cac","#c19cbc", "#b29cc1", "#9c9dc1",
