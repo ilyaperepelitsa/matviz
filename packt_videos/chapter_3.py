1dce221843fc04d84ac05bd4c7f2d3b79a54d73a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 15:24:05 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -134,13 +134,6 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                             xlabel = "Label X axis",
                             ylabel = "Label Y axis"):
 
-    # color_cycler = cycle(plt.rc("axes",
-    #                         prop_cycle = (cycler("color", ["#54637a", "#54787a",
-    #                                     "#547a63", "#6b7a54", "#918e5f",
-    #                                     "#b29563", "#bf8765", "#bf7265",
-    #                                     "#aa4747", "#824a53", "#824a6b",
-    #                                     "#603f63", "#4e3f63"]))))
-    #
     color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
     # # cmap = lambda x: [x, next(color_cycler)["color"]]
     cmap = lambda: next(color_cycler)["color"]
