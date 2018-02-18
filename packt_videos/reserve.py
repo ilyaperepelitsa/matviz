ae036eb7e888c405b53a26bedddd526d2d2da95f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 01:43:09 -0500

    Updated reserve.py.

--- packt_videos/reserve.py
+++ packt_videos/reserve.py
@@ -5,3 +5,8 @@
          # print(data_data[0])
          # plot_cols[str(i)] = cmap(str(i))[1]
          plot_cols[data_data[0]] = cmap(data_data[0])[1]
+
+
+
+color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
+cmap = lambda x: [x, next(color_cycler)["color"]]
