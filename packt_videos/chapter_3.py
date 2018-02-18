43e0c66530930468feed626beef37ee097fb7c9e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 14:54:11 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -183,7 +183,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
 
     plot_cols = dict()
     for patch in boxes["boxes"]:
-        patch.set_facecolor()
+        patch.set_facecolor(cmap())
          # plot_cols[data_data[0]] = cmap(data_data[0])[1]
          # print(cmap())
 
