aa75498a04e2a051864598ad80584c74e62f02c6
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 15:18:21 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -211,11 +211,11 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
                 )
 
-    # plot_cols = dict()
-    # for patch in boxes["boxes"]:
-    #     patch.set_facecolor(cmap())
-    #      # plot_cols[data_data[0]] = cmap(data_data[0])[1]
-    #      # print(cmap())
+    plot_cols = dict()
+    for patch in boxes["boxes"]:
+        patch.set_facecolor(cmap())
+         # plot_cols[data_data[0]] = cmap(data_data[0])[1]
+         # print(cmap())
 
     # print(pew["boxes"])
 
