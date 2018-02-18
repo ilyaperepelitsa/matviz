2d5f50457c507a5934e5d1d2778837c6e899f1fe
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 01:44:43 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -269,7 +269,7 @@ plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
                     data_group = "starting_street", invert_axes = True,
                     order = "desc", order_by = "iqr", show_outliers = False,
                     get_top = 3,
-                    subtitle = "5 Most frequent Starting streets for 30k taxi trips. Time per maneuver.",
+                    subtitle = "3 Most frequent Starting streets for 30k taxi trips. Time per maneuver.",
                     ylabel = "Trip starting street",
                     xlabel = "Travel time per maneuver")
 
