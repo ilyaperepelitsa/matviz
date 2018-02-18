1f6b2f82b6cc7adc507b744d15c65e7361a5d596
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 16:58:56 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -13,10 +13,8 @@ from cycler import cycler
 from collections import defaultdict
 from random import shuffle
 
-palette = [['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53',
- '#54787a',
- '#918e5f',
- '#bf8765',
+palette = [['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
+ '#918e5f','#bf8765',
  '#4e3f63',
  '#824a6b',
  '#547a63',
