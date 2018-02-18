a35a9bd26859c4110416d86e3cc206edfb28f4d8
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 15:23:14 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -196,6 +196,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
         invert_value = 1
 
     matplotlib.rc('axes', edgecolor=keycolor)
+    palette = 
     plt.rc("axes", prop_cycle = (cycler("color", ["#54637a", "#54787a",
                                         "#547a63", "#6b7a54", "#918e5f",
                                         "#b29563", "#bf8765", "#bf7265",
