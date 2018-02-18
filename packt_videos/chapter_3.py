a97a4e07e88b2bb44564e265811f9a7eccacd316
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 16:24:17 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -201,10 +201,12 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
         invert_value = 1
 
     matplotlib.rc('axes', edgecolor=keycolor)
-    palette = ["#54637a", "#54787a","#547a63", "#6b7a54", "#918e5f",
-                "#b29563", "#bf8765", "#bf7265","#aa4747", "#824a53",
-                "#824a6b", "#603f63", "#4e3f63"]
-    
+    # palette = ["#54637a", "#54787a","#547a63", "#6b7a54", "#918e5f",
+    #             "#b29563", "#bf8765", "#bf7265","#aa4747", "#824a53",
+    #             "#824a6b", "#603f63", "#4e3f63"]
+    palette = ["#c19c9e", "#c19cac","#c19cbc", "#b29cc1", "#9c9dc1",
+                "#9cb0c1", "#9cc1bd", "#9cc1a2","#bcc19c", "#c1b19c",
+                "#c1a09c"]
     shuffle(palette)
     plt.rc("axes", prop_cycle = (cycler("color", palette)))
     boxes = plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
