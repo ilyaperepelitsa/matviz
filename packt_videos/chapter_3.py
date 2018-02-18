2032bb5347ff28614d92b21b86b248a114ce7847
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 17:11:02 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -264,8 +264,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
 plot_boxplots_groups(data_in = sample_30k, data_num = "travel_time_per_step",
                     data_group = "starting_street", invert_axes = True,
                     order = "desc", order_by = "iqr", show_outliers = False,
-                    get_top = 12,
-                    subtitle = "12 Most frequent Starting streets for 30k taxi trips. Time per maneuver.",
+                    get_top = 20,
+                    subtitle = "20 Most frequent Starting streets for 30k taxi trips. Time per maneuver.",
                     ylabel = "Trip starting street",
                     xlabel = "Travel time per maneuver")
 
