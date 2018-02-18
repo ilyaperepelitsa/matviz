cfc3144bb1b92e099d115c2ecfd1d3f3162b3427
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 15:18:38 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -141,7 +141,7 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
     #
     # # color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
     # # cmap = lambda x: [x, next(color_cycler)["color"]]
-    # cmap = lambda: next(color_cycler)["color"]
+    cmap = lambda: next(color_cycler)["color"]
 
     labels_to_filter = data_in.groupby([data_group]).size().sort_values(ascending = False).index
     if isinstance(int(get_top), int):
