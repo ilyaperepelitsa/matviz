45d2d30e9451d05e3354d454b2e8f1495e0f91ee
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-18 19:12:59 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -20,7 +20,7 @@ palette_pastel = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#
             '#c1b19c','#b29cc1','#9cc1bd','#9cc1a2']
 
 pew = plt.rc("axes", prop_cycle = (cycler("color", palette_pastel)))
-
+pew
 cmap = lambda: next(pew)["color"]
 cmap()
 
