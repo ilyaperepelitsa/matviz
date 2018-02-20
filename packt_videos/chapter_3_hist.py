0751ca7b297bbf70dda100de4e10127a8fe7031a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 14:22:47 -0500

    Updated chapter_3_hist.py.

--- packt_videos/chapter_3_hist.py
+++ packt_videos/chapter_3_hist.py
@@ -35,7 +35,7 @@ cmap = lambda x: [x, next(color_cycler)["color"]]
 # LOADING DATA
 #####################
 
-elec_2013 = pd.read_csv("~/Downloads/")
+elec_2013 = pd.read_csv("~/Downloads/israeli")
 
 
 labels = []
