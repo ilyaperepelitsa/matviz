47d9f4370a8b3789f6686500e2278265c3734400
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 08:00:54 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -332,9 +332,9 @@ plt.rc("axes", prop_cycle = (cycler("color", ["#547a63", "#4e3f63", "#aa4747"]))
 plt.rcParams['axes.facecolor'] = '#252628'
 for street in sample_1k["starting_street"].unique():
     # pew.append(cmap(street))
-    # plt.scatter(sample_1k.loc[sample_1k["starting_street"] == street, "lon"],
-    #             sample_1k.loc[sample_1k["starting_street"] == street, "lat"],
-    #             c = cmap(street),alpha = 0.5, edgecolor = "none", linewidth = 0.5)
+    plt.scatter(sample_1k.loc[sample_1k["starting_street"] == street, "lon"],
+                sample_1k.loc[sample_1k["starting_street"] == street, "lat"],
+                c = cmap(street)[],alpha = 0.5, edgecolor = "none", linewidth = 0.5)
 pew
 
 
