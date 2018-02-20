7535db188584d90dc718b8c4e3d10a35d3c2fbdc
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 14:22:06 -0500

    Updated chapter_3_hist.py.

--- packt_videos/chapter_3_hist.py
+++ packt_videos/chapter_3_hist.py
@@ -16,7 +16,9 @@ from random import shuffle
 
 
 #####################
-STYLE DEFINITI
+# STYLE DEFINITIONS
+#####################
+
 palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
 
