d89bfe0a6e1d461d51fc43350fc78386a35bc818
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 08:37:22 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -344,5 +344,5 @@ legend_handlers = [plt.scatter([], [], marker = "o", label = label_entry[])]
 plt.axes().set_aspect("equal")
 plt.show()
 
-for i.items() in labels:
+for i in labels:
     print(i.keys())
