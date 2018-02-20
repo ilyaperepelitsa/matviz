d5569c66c698dc59c341733f6778f2df7d1bcff6
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 08:31:55 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -324,10 +324,12 @@ palette23 = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#9cb0c
 palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
 
-pew = []
+
 # cmap = lambda: next(color_cycler)["color"]
 cmap = lambda x: {x: next(color_cycler)["color"]}
 
+
+labels = []
 plt.figure(figsize = (10, 10))
 plt.rc("axes", prop_cycle = (cycler("color", ["#547a63", "#4e3f63", "#aa4747"])))
 plt.rcParams['axes.facecolor'] = '#252628'
