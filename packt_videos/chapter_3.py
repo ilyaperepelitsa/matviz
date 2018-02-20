a0e061118d0aff13c33aff2f5074b9f6448154fa
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 14:12:04 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -348,14 +348,14 @@ plt.legend(handles = legend_handlers, scatterpoints = 1, ncol = 3, loc = 'upper 
 plt.axes().set_aspect("equal")
 plt.show()
 
-
+labels = []
 sample_trips = np.random.choice(total_exploded["starting_street"].unique(), 3)
 sample_1k = total_exploded.loc[total_exploded["starting_street"].isin(sample_trips), :]
 labels
 for street in sample_1k["starting_street"].unique():
     labels.append(cmap(street))
 
-    
+
 for i in labels:
     plt.hist(sample_1k.loc[sample_1k["starting_street"] == street[0], "lon"], color = street[1], histtype = "barstacked", bins = 20)
     # print(sample_1k.loc[sample_1k["starting_street"] == street[0], "lon"].shape)
