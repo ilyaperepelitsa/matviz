7eec459a5563c2dc2aa8e2f0995c0d60f399b3b5
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 08:00:16 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -335,7 +335,7 @@ for street in sample_1k["starting_street"].unique():
     # plt.scatter(sample_1k.loc[sample_1k["starting_street"] == street, "lon"],
     #             sample_1k.loc[sample_1k["starting_street"] == street, "lat"],
     #             c = cmap(street),alpha = 0.5, edgecolor = "none", linewidth = 0.5)
-
+pew
 
 
 plt.axes().set_aspect("equal")
