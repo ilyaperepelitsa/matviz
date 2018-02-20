5b4f6fe52fdd1f905ee7f2ae4a510f3d70024662
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 14:21:54 -0500

    Updated chapter_3_hist.py.

--- packt_videos/chapter_3_hist.py
+++ packt_videos/chapter_3_hist.py
@@ -13,6 +13,9 @@ from cycler import cycler
 from collections import defaultdict
 from random import shuffle
 
+
+
+#########
 palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
 
@@ -20,8 +23,6 @@ palette_pastel = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#
             '#c1b19c','#b29cc1','#9cc1bd','#9cc1a2']
 
 plt.rc("axes", prop_cycle = (cycler("color", palette)))
-
-
 color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
 cmap = lambda x: [x, next(color_cycler)["color"]]
 
