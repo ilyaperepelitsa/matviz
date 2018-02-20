50c09e3dba26a846773b79ad95dfe94bf77613c3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 08:31:49 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -325,7 +325,7 @@ palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a'
             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
 
 pew = []
-cmap = lambda: next(color_cycler)["color"]
+# cmap = lambda: next(color_cycler)["color"]
 cmap = lambda x: {x: next(color_cycler)["color"]}
 
 plt.figure(figsize = (10, 10))
