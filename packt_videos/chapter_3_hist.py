7ebbc66dcf27b1c060a2117b93a13658afa46d55
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 14:21:15 -0500

    Updated chapter_3_hist.py.

--- packt_videos/chapter_3_hist.py
+++ packt_videos/chapter_3_hist.py
@@ -23,23 +23,7 @@ plt.rc("axes", prop_cycle = (cycler("color", palette)))
 
 
 color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
-# # cmap = lambda x: [x, next(color_cycler)["color"]]
-cmap = lambda: next(color_cycler)["color"]
-cmap = lambda x: {x, next(color_cycler)["color"]}
-# plot_cols = dict()
-# for patch in boxes["boxes"]:
-#     patch.set_facecolor(cmap())
 
-
-palette23 = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#9cb0c1',
-            '#c1b19c','#b29cc1','#9cc1bd','#9cc1a2']
-
-
-palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
-            '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
-
-
-# cmap = lambda: next(color_cycler)["color"]
 cmap = lambda x: [x, next(color_cycler)["color"]]
 
 labels
