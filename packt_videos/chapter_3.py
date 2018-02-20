2e7a76f9b8aa6647650cdd5aff625a9f02d404ed
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-02-20 07:52:25 -0500

    Updated chapter_3.py.

--- packt_videos/chapter_3.py
+++ packt_videos/chapter_3.py
@@ -324,7 +324,7 @@ palette23 = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#9cb0c
 palette = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
 
-зуц =
+pew = []
 cmap = lambda: next(color_cycler)["color"]
 plt.figure(figsize = (10, 10))
 plt.rc("axes", prop_cycle = (cycler("color", ["#547a63", "#4e3f63", "#aa4747"])))
