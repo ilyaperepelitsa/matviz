f2de43bb2beee99fd602c48a0075d1dd19ef0864
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2018-11-04 00:11:06 +1000

    Updated chapter_1.pyarchitecture .

--- MASTERING_MATPLOTLIB/chapter_1.py
+++ MASTERING_MATPLOTLIB/chapter_1.py
@@ -1,2 +1,12 @@
 import matplotlib
 matplotlib.use('nbagg')
+
+
+import numpy as np
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        from matplotlib.backends import backend_agg
+        from matplotlib.colors import LinearSegmentedColormap
+        from matplotlib.gridspec import GridSpec
+        import seaborn as sns
+        from IPython.display import Image
