b019e634d770ebc500209bdb7d1cefdcc934e154
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2018-11-03 23:57:13 +1000

    Updated chapter_1.pyarchitecture .

--- MASTERING_MATPLOTLIB/chapter_1.py
+++ MASTERING_MATPLOTLIB/chapter_1.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
-plt.rcParams['backend']
-plt._backend_mod.__name__
-plt._show
-
-
-plt._pylab_helpers.Gcf
-matplotlib._pylab_helpers.Gcf
-plt._pylab_helpers.Gcf.get_active()
-plt.get_current_fig_manager()
-plt.get_current_fig_manager().figure
-plt.plot([1,2,3,4])
-plt._pylab_helpers.Gcf.get_active()
-plt._pylab_helpers.Gcf.get_active().canvas
+# plt.rcParams['backend']
+# plt._backend_mod.__name__
+# plt._show
+#
+#
+# plt._pylab_helpers.Gcf
+# matplotlib._pylab_helpers.Gcf
+# plt._pylab_helpers.Gcf.get_active()
+# plt.get_current_fig_manager()
+# plt.get_current_fig_manager().figure
+# plt.plot([1,2,3,4])
+# plt._pylab_helpers.Gcf.get_active()
+# plt._pylab_helpers.Gcf.get_active().canvas
