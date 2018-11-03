e4e5ec3ef3d92938acec38bd3aaac1ca2b452fbe
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2018-11-04 00:11:18 +1000

    Updated chapter_1.pyarchitecture .

--- MASTERING_MATPLOTLIB/chapter_1.py
+++ MASTERING_MATPLOTLIB/chapter_1.py
@@ -3,10 +3,17 @@ matplotlib.use('nbagg')
 
 
 import numpy as np
-        import matplotlib.pyplot as plt
-        import matplotlib as mpl
-        from matplotlib.backends import backend_agg
-        from matplotlib.colors import LinearSegmentedColormap
-        from matplotlib.gridspec import GridSpec
-        import seaborn as sns
-        from IPython.display import Image
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.backends import backend_agg
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
+from IPython.display import Image
+
+
+pallete_name = "husl"
+        colors = sns.color_palette(pallete_name, 8)
+        colors.reverse()
+        cmap = mpl.colors.LinearSegmentedColormap.from_list(
+            pallete_name, colors)
