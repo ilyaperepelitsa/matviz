0404b31f8601e3e03b2977260e14dc3641781f58
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2018-11-03 19:10:41 +1000

    Updated boxplot.py.

--- custom_graphs/boxplot.py
+++ custom_graphs/boxplot.py
@@ -1,1 +1,126 @@
 
+
+
+def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
+                            order = "desc", order_by = "median",
+                            show_outliers = True, get_top = False,
+                            keycolor = "#232626",
+                            # title = "Don't forget your titles.",
+                            subtitle = "Don't forget your subtitles.",
+                            xlabel = "Label X axis",
+                            ylabel = "Label Y axis"):
+
+    color_cycler = cycle(plt.rcParams["axes.prop_cycle"])
+    # # cmap = lambda x: [x, next(color_cycler)["color"]]
+    cmap = lambda: next(color_cycler)["color"]
+
+    labels_to_filter = data_in.groupby([data_group]).size().sort_values(ascending = False).index
+    if isinstance(int(get_top), int):
+        group_labels = labels_to_filter[0:int(get_top)]
+
+    if get_top == False:
+        group_labels = labels_to_filter.tolist()
+
+    if invert_axes == False:
+        canvas_width = 0.4*len(group_labels)
+        canvas_height = 13
+    else:
+        canvas_width = 13
+        canvas_height = 0.4*len(group_labels)+ 0.8
+
+    plt.figure(figsize = (canvas_width, canvas_height))
+    if order_by == "mean":
+        data = [[data_group_var,
+            [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
+            data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].mean().values[0]]
+            for data_group_var in group_labels]
+
+    elif order_by == "iqr":
+        data = [[data_group_var,
+            [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
+            (data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].quantile(.75) - \
+            data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].quantile(.25)).values[0]]
+            for data_group_var in group_labels]
+
+    elif order_by == "median":
+        data = [[data_group_var,
+            [data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].values],
+            data_in.loc[data_in[data_group].isin([data_group_var]), [data_num]].median().values[0]]
+            for data_group_var in group_labels]
+
+
+    if order == "desc":
+        data_sorted = sorted(data, key=lambda x: x[2])
+    else:
+        data_sorted = sorted(data, key=lambda x: x[2], reverse = True)
+
+    if show_outliers:
+        outlier_mark = "c"
+    else:
+        outlier_mark = ""
+
+    if invert_axes:
+        invert_value = 0
+    else:
+        invert_value = 1
+
+    matplotlib.rc('axes', edgecolor=keycolor)
+    # palette23 = ['#aa4747','#bf7265','#b29563','#54637a','#603f63','#824a53','#54787a',
+    #             '#918e5f','#bf8765','#4e3f63','#824a6b','#547a63','#6b7a54']
+
+    palette23 = ['#bcc19c','#c19c9e','#c19cbc','#c1a09c','#c19cac','#9c9dc1','#9cb0c1',
+                '#c1b19c','#b29cc1','#9cc1bd','#9cc1a2']
+
+    plt.rc("axes", prop_cycle = (cycler("color", palette23)))
+    boxes = plt.boxplot([data_data[1] for data_data in data_sorted], 0, outlier_mark, invert_value,
+                patch_artist = True,
+                whiskerprops = {'color': "#9ea5a8", 'linewidth' : 9.5, 'zorder' : 0},
+                medianprops = {'color': "#ffffff", 'linewidth' : 3, 'zorder' : 1000},
+                # capprops = {'color': keycolor, 'linewidth' : 4},
+                showcaps=False,
+
+                boxprops = {'color': keycolor, 'facecolor': "#5e5757", 'zorder' : 999}
+                # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
+                )
+    cmap = lambda: next(color_cycler)["color"]
+    # plot_cols = dict()
+    for patch in boxes["boxes"]:
+        patch.set_facecolor(cmap())
+         # plot_cols[data_data[0]] = cmap(data_data[0])[1]
+         # print(cmap())
+
+    # print(pew["boxes"])
+
+    names = [data_data[0] for data_data in data_sorted]
+    if invert_axes:
+        plt.yticks(range(1, len(names) +1), names, color=keycolor)
+    else:
+        plt.xticks(range(1, len(names) +1), names, color=keycolor)
+
+
+    # Y axis label
+    plt.ylabel(ylabel, color=keycolor,
+               fontsize=12)
+    # Y axis label
+    plt.xlabel(xlabel, color=keycolor,
+               fontsize=12)
+
+    # Graph title
+    # plt.suptitle(title,
+    #             y=1.025, x = 0.12,
+    #             # horizontalalignment="right",
+    #             horizontalalignment="left",
+    #             # horizontalalignment="center",
+    #              fontsize=18, color=keycolor,
+    #              )
+    # plt.text(y=2, x = 0.12,
+    #         s = subtitle, horizontalalignment = "left", fontsize=14, color=keycolor)
+
+    # Graph subtitle
+    plt.title(subtitle,
+                y=1.01 , x = 0,
+                 loc = "left", fontsize=18,
+                color=keycolor)
+
+    plt.savefig('/Users/ilyaperepelitsa/quant/pewpewpew231.jpg', dpi=300)
+    plt.show()
