bb5aef4ccab66bc8e4afc5ac4ea945760e8b8d76
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2018-11-03 19:10:54 +1000

    Updated boxplot.py.

--- custom_graphs/boxplot.py
+++ custom_graphs/boxplot.py
@@ -83,13 +83,8 @@ def plot_boxplots_groups(data_in, data_num, data_group, invert_axes = False,
                 # boxprops = {'color': keycolor, 'facecolor': keycolor, 'zorder' : 999}
                 )
     cmap = lambda: next(color_cycler)["color"]
-    # plot_cols = dict()
     for patch in boxes["boxes"]:
         patch.set_facecolor(cmap())
-         # plot_cols[data_data[0]] = cmap(data_data[0])[1]
-         # print(cmap())
-
-    # print(pew["boxes"])
 
     names = [data_data[0] for data_data in data_sorted]
     if invert_axes:
